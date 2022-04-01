a9a2c63b833df529d36fb73ff781a9ccb07ba2bb
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-01 13:48:39 +0100

    Add function to add a new pet to a given customer

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -45,3 +45,6 @@ def remove_customer_cash(customer, amount):
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet_data):
+    customer["pets"].append(pet_data)
